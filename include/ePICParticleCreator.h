c97217d03cebc0fa56ccb304c86eee85c5f1c8d6
Derek Glazier <Derek.Glazier@glasgow.ac.uk>
2025-06-04 10:03:00 +0100

    remove hard coded pprime and make it default instead for RP and B0

--- include/ePICParticleCreator.h
+++ include/ePICParticleCreator.h
@@ -42,26 +42,16 @@ namespace rad{
     /// functions which depend on other particles
     template<typename Tp, typename Tm>
       int ParticleMCMatched(const int idx,const RVec<Tp> &tpx,const  RVec<Tp> &tpy,const  RVec<Tp> &tpz, const Tm &tmass, RVec<Tp> &px, RVec<Tp> &py, RVec<Tp> &pz, RVec<Tp> &m,const RVecI& iafter){
-      
+      std::cout<<"ParticleMCMatched "<<m<<" "<<tpz<<std::endl;
       //add new components
       if(tpx.empty()==false){
-	//std::cout << "Old momenta         " <<idx<<" "<<pz[idx]<<" "<<m[idx]<<" "<<std::endl; 
 	px[idx]=tpx[0];
 	py[idx]=tpy[0];
 	pz[idx]=tpz[0];
 	m[idx] = tmass;
-	//m[idx] = 0.00051099900;
-	
-	//std::cout<<"ParticleMCMatched "<<idx<<" "<<pz[idx]<<" "<<m[idx]<<" \n"<<std::endl;
 	return idx;
       }
-      else{
-	//std::cout << "Container Empty         " <<idx<<" "<<pz[idx]<<" "<<m[idx]<<" \n"<<std::endl; 
-	//px[idx]=0;
-	//py[idx]=0;
-	//pz[idx]=0;
-	//m[idx] = tmass;
-	
+      else{	
 	return -1;
       }
     }
@@ -101,32 +91,32 @@ namespace rad{
 	
       }
       
-      void RomanPotProton() {
+      void RomanPotProton(const std::string name="pprime") {
 	Reaction()->setBranchAlias("ForwardRomanPotRecParticles.momentum.x","rp_px");
 	Reaction()->setBranchAlias("ForwardRomanPotRecParticles.momentum.y","rp_py");
 	Reaction()->setBranchAlias("ForwardRomanPotRecParticles.momentum.z","rp_pz");
 	
 	//cant find a way to use this yet
 	//float proton_mass=0.93827208943;
-	Reaction()->Define(Rec()+"pprime",Form("rad::epic::Particle(rp_px,rp_py,rp_pz,0.93827208943,%spx,%spy,%spz,%sm,{0})",Rec().data(),Rec().data(),Rec().data(),Rec().data()));
+	Reaction()->Define(Rec()+name,Form("rad::epic::Particle(rp_px,rp_py,rp_pz,0.93827208943,%spx,%spy,%spz,%sm,{0})",Rec().data(),Rec().data(),Rec().data(),Rec().data()));
 	//copy rec_pprime to pprime?
-	//Reaction()->Define("pprime",Rec()+"pprime");
-	Reaction()->AddParticleName(Rec()+"pprime");
+	//Reaction()->Define(name,Rec()+name);
+	Reaction()->AddParticleName(Rec()+name);
       }
-      void MCMatchedRomanPotProton() {
+      void MCMatchedRomanPotProton(const std::string name="pprime") {
 	Reaction()->setBranchAlias("ForwardRomanPotRecParticles.momentum.x","rp_px");
 	Reaction()->setBranchAlias("ForwardRomanPotRecParticles.momentum.y","rp_py");
 	Reaction()->setBranchAlias("ForwardRomanPotRecParticles.momentum.z","rp_pz");
 	
-	Reaction()->Define(Rec()+"RPproton",Form("rad::epic::ParticleMCMatched(%s,rp_px,rp_py,rp_pz,0.93827208943,%spx,%spy,%spz,%sm,{0})","pprime",Rec().data(),Rec().data(),Rec().data(),Rec().data()));
+	Reaction()->Define(Rec()+"RPproton",Form("rad::epic::ParticleMCMatched(%s,rp_px,rp_py,rp_pz,0.93827208943,%spx,%spy,%spz,%sm,{0})",name,Rec().data(),Rec().data(),Rec().data(),Rec().data()));
 	Reaction()->AddParticleName(Rec()+"RPproton");
       }
-      void MCMatchedB0Proton() {
+      void MCMatchedB0Proton(const std::string name="pprime") {
 	Reaction()->setBranchAlias("ReconstructedTruthSeededChargedParticles.momentum.x","B0_px");
 	Reaction()->setBranchAlias("ReconstructedTruthSeededChargedParticles.momentum.y","B0_py");
 	Reaction()->setBranchAlias("ReconstructedTruthSeededChargedParticles.momentum.z","B0_pz");
 	
-	Reaction()->Define(Rec()+"B0proton",Form("rad::epic::ParticleMCMatched(%s,B0_px,B0_py,B0_pz,0.93827208943,%spx,%spy,%spz,%sm,{0})","pprime",Rec().data(),Rec().data(),Rec().data(),Rec().data()));
+	Reaction()->Define(Rec()+"B0proton",Form("rad::epic::ParticleMCMatched(%s,B0_px,B0_py,B0_pz,0.93827208943,%spx,%spy,%spz,%sm,{0})",name,Rec().data(),Rec().data(),Rec().data(),Rec().data()));
 	Reaction()->AddParticleName(Rec()+"B0proton");
       }
       
